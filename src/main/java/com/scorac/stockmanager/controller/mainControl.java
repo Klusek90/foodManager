753381123b0e09611e54ddaa977a6190436040ff
gkluz <gkluz@datagraphic.co.uk>
2024-01-15 09:49:10 +0000

    endpoints

---
+++ src/main/java/com/scorac/stockmanager/controller/mainControl.java
@@ -0,0 +1,37 @@
+package com.scorac.stockmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainControl {
+
+    @GetMapping({"/","home","index"})
+    public String homePage(){
+        return "index";
+    }
+
+    @GetMapping("/users")
+    public String manageUsers(){
+        return "users";
+    }
+    @GetMapping("/stock")
+    public String stockContorl(){
+        return "stock";
+    }
+
+    @GetMapping("/recipes")
+    public String recipes(){
+        return "recipes";
+    }
+
+    @GetMapping("/solution")
+    public String queryChat(){
+        return "queryGPT";
+    }
+
+//    @GetMapping("/")
+//    public String (){
+//        return "";
+//    }
+}
