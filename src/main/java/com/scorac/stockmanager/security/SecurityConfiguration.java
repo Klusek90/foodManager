8802ad8c5abd4d7ef7b8bf70de2af77c5128e89e
gkluz <gkluz@datagraphic.co.uk>
2024-02-06 13:08:33 +0000

    fixing login form and layout

--- src/main/java/com/scorac/stockmanager/security/SecurityConfiguration.java
+++ src/main/java/com/scorac/stockmanager/security/SecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,6 +19,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authz -> authz
+                        .requestMatchers("/loginPage.css").permitAll()
                         .requestMatchers("/users/*", "/reports", "/recipes").hasRole("ADMIN") // only for admin
                         .anyRequest().authenticated())
                 .formLogin(form -> form
@@ -26,6 +29,10 @@ public class SecurityConfiguration {
         return http.build();
     }
 
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().requestMatchers("/static/loginPage.css");
+//    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
