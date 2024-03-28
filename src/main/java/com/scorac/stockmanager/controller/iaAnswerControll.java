19db090a3862c091b49c622dd52d4c103c438c2c
gkluz <gkluz@datagraphic.co.uk>
2024-03-28 10:44:06 +0000

    adding chat

--- src/main/java/com/scorac/stockmanager/controller/iaAnswerControll.java
+++ src/main/java/com/scorac/stockmanager/controller/iaAnswerControll.java
@@ -1,23 +1,26 @@
-//package com.scorac.stockmanager.controller;
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.ai.chat.ChatClient;
-//
-//@RestController
-//@RequestMapping("/question")
-//public class iaAnswerControll {
-//
-//
-//    private final ChatClient chatClient;
-//
-//    public iaAnswerControll(ChatClient chatClient){
-//        this.chatClient = chatClient;
-//    };
-//    @GetMapping("/request")
-//    public String request(){
-////        String respond = chatClient.generate("tell me something about yourself");
-//        return chatClient.generate("tell me something about yourself");
-//    }
-//}
+package com.scorac.stockmanager.controller;
+
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.openai.OpenAiChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/question")
+public class iaAnswerControll {
+
+    private final OpenAiChatClient chatClient;
+
+    @Autowired
+    public iaAnswerControll(OpenAiChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/request")
+    public String request(){
+        String requst = "tell me something about yourself";
+        return chatClient.call(requst);
+    }
+}
