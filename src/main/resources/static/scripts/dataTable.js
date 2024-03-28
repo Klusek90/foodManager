960969b24a71b5dbd67662034129a1308383da92
gkluz <gkluz@datagraphic.co.uk>
2024-03-28 12:49:28 +0000

    fixing table

--- src/main/resources/static/scripts/dataTable.js
+++ src/main/resources/static/scripts/dataTable.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
     $('#all-table').DataTable({
         "processing": true,
+        "responsive": true, // Enable the responsive option
         "ajax": {
             "url": "http://localhost:8080/stock/datatable",
             "type": "GET",
-            // Ensure proper data handling, assuming your endpoint returns a plain array
             "dataSrc": ""
         },
         "columns": [
-            {data: "name", "width": "20%"},
-            {data: "type", "width": "20%"},
-            {data: "weight", "width": "20%"},
-            {data: "cost", "width": "20%"},
-            {data: "other", "width": "20%"}
+            {data: "name", "width": "15%"},
+            {data: "type", "width": "15%"},
+            {data: "weight", "width": "15%"},
+            {data: "cost", "width": "15%"},
+            {data: "other", "width": "15%"}
         ]
     });
 });
