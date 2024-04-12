36318a70266f7dcc4bb430d9d2850584f469b3f9
gkluz <gkluz@datagraphic.co.uk>
2024-04-12 16:23:32 +0100

    upload Weka

---
+++ src/main/java/com/scorac/stockmanager/service/WekaService.java
@@ -0,0 +1,9 @@
+package com.scorac.stockmanager.service;
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+
+public class WekaService {
+
+}
