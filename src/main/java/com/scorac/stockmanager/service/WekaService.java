103dc8ee0f62bc1559d2a160b069fe4c6c438fa8
gkluz <gkluz@datagraphic.co.uk>
2024-04-12 17:35:08 +0100

    createing bigData

--- src/main/java/com/scorac/stockmanager/service/WekaService.java
+++ src/main/java/com/scorac/stockmanager/service/WekaService.java
@@ -4,11 +4,14 @@ import com.scorac.stockmanager.model.Entity.*;
 import com.scorac.stockmanager.service.Repository.*;
 import jakarta.persistence.Access;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.stereotype.Service;
 import weka.classifiers.Classifier;
 import weka.core.Instances;
 import java.io.ObjectOutputStream;
 import java.io.FileOutputStream;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,12 +26,23 @@ public class WekaService {
     private BookingRepository bookingRepository;
 
     public List<BigData> bigDataSet(){
+        LocalDate today = LocalDate.now();
+        LocalDate previusDate;
 
         List<Weather> weathers = weatherRepository.findAll();
         List<Made> preparationMde= prepCreatedRepository.findAll();
         List<Sale> sales= saleRepository.findAll();
         List<Booking> bookings = bookingRepository.findAll();
         List<Waste> wastes = wasteRepository.findAll();
+
+        //2 years data
+        for(int i =0; i<730; i++){
+            previusDate = today.minusDays(i);
+            if()
+
+
+        }
+
 
         List<BigData> dataset= new ArrayList<>();
 
